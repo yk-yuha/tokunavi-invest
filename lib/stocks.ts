68c564efc37dc06914da18128e38e2ec41d4eff9
yk-yuha <y.koinuma@yuhakanoko.co.jp>
2026-01-17 02:00:28 +0900

    add rights months to stocks

--- lib/stocks.ts
+++ lib/stocks.ts
@@ -1,12 +1,24 @@
 export type Stock = {
   code: string;
   name: string;
+  months: number[]
   yield: number;
   benefit: string;
 };
 
-export const stocks: Stock[] = [
-  { code: "7203", name: "トヨタ自動車", yield: 2.5, benefit: "QUOカード" },
-  { code: "9432", name: "日本電信電話（NTT）", yield: 3.2, benefit: "dポイント" },
-  { code: "8267", name: "イオン", yield: 1.6, benefit: "買物優待券" },
+export const stocks = [
+  {
+    code: "7203",
+    name: "トヨタ自動車",
+    yield: 2.5,
+    benefit: "QUOカード",
+    months: [3, 9],
+  },
+  {
+    code: "9432",
+    name: "NTT",
+    yield: 3.1,
+    benefit: "dポイント",
+    months: [3],
+  },
 ];
